2997a43ec3f41c44ef6c77cfbd0c7d2392280844
Zack Seuberling <seuberling@gmail.com>
2017-06-04 11:49:22 -0700

    Update About section

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,9 @@ class About extends React.Component {
         <div className="close-about">
           <a href="#" onClick={this.props.toggleAbout}>&times;</a>
         </div>
-
-        <p>Over the past couple months, I have been compiling a list of companies in Seattle that are focused in the design industry. My desire came out of finding a job, but as I continued to research, I kept running into unreliable and out dated information. You will not find companies that have dedicated in house design or development jobs (Amazon, Microsoft, etc.) because those are easy to find.</p>
-        <p><strong>Seattle Design</strong> is the result of my  research. It's still a work in progress, but I hope it helps those who find themselves in the same situation that I was in.</p>
-        <p>To correct any out of date information please <a className="inverse-anchor" href="mailto:info@mail.seattledesign.info">notify me</a> about any changes that should be made.</p>
+        <p><strong>Seattle Design</strong> is the result of months of research to uncover the thriving Seattle graphic and interactive design community. This is a work in progress, but my hope is that it helps students, city transplants, hiring managers, and those looking for a job.</p>
+        <p><strong>Seattle Design</strong> grew out of a frustration while researching the Seattle Design scene. Information online was almost always out-of-date or undiscoverable. Companies I knew existed were nearly impossible to find through LinkedIn and Google queries. I had to believe other people had a similar kind of frustration.</p>
+        <p>This is a labor of love, so please help me out to correct any information by <a className="inverse-anchor" href="mailto:info@mail.seattledesign.info">notifying me</a> of changes that should be made.</p>
       </div>
     )
   }
